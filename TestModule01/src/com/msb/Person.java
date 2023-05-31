03e40c36dc46d07a31a8e3d422510c71b37919df
wangshaofan <3414391918@qq.com>
2023-05-31 22:27:39 +0800

    提交Person

---
+++ TestModule01/src/com/msb/Person.java
@@ -0,0 +1,12 @@
+package com.msb;
+
+/**
+ * @Auther: 王少凡
+ * @Date: 2023/5/31 - 05 - 31 - 22:21
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Person {
+    private String name;
+    private Integer age;
+}
